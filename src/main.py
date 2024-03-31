053053a63543fe60dff2b8304b9d288197b4de91
Thibaut Monin <tmonin@pageup.fr>
2024-03-31 23:21:33 +0200

    restart game feature

--- src/main.py
+++ src/main.py
@@ -37,6 +37,30 @@ background_path = os.path.join(dirname, "../assets/background.png")
 floor_path = os.path.join(dirname, "../assets/floor.png")
 background = pygame.image.load(background_path)
 floor = pygame.image.load(floor_path)
+restart_image_path = os.path.join(dirname, "../assets/restart.png")
+restart_image = pygame.image.load(restart_image_path)
+
+class Button():
+  def __init__(self, x, y, image):
+    self.image = image
+    self.rect = self.image.get_rect()
+    self.rect.topleft = (x, y)
+
+  def draw(self):
+    action = False
+    pos = pygame.mouse.get_pos()
+    if self.rect.collidepoint(pos) and pygame.mouse.get_pressed()[0] == 1:
+        action = True
+
+    screen.blit(self.image, (self.rect.x, self.rect.y))
+    return action
+  
+def reset_game():
+  pipe_group.empty()
+  flappy.rect.x = 100
+  flappy.rect.y = int(screen_height / 2)
+  score = 0
+  return score
 
 def draw_text(text, font, text_color, x, y):
   img = font.render(text, True, text_color)
@@ -47,6 +71,7 @@ pipe_group = pygame.sprite.Group()
 
 flappy = bird.Bird(100, int(screen_height / 2))
 bird_group.add(flappy)
+restart_button = Button(screen_width / 2 - 50, screen_height / 2 - 100, restart_image)
 
 run = True
 
@@ -99,6 +124,11 @@ while run:
     if abs(scroll) > 35:
       scroll = 0
     pipe_group.update()
+
+  # Check for gameover and reset
+  if game_over == True and restart_button.draw() == True:
+      game_over = False
+      score = reset_game()
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
