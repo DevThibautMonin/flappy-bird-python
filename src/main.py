68b33e02f00e2b5ff7475ce344cb34ddca20ebe2
Thibaut Monin <tmonin@pageup.fr>
2024-03-31 20:05:34 +0200

    infinite scrolling floor

--- src/main.py
+++ src/main.py
@@ -3,22 +3,37 @@ import os
 
 pygame.init()
 
-screen_width = 800
-screen_height = 600
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 850
+screen_height = 850
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Bird")
 
-run = True
+scroll = 0
+scroll_speed = 4
 
 # Load images
 dirname = os.path.dirname(__file__)
 background_path = os.path.join(dirname, "../assets/background.png")
+floor_path = os.path.join(dirname, "../assets/floor.png")
 background = pygame.image.load(background_path)
+floor = pygame.image.load(floor_path)
+
+run = True
 
 while run:
 
+  clock.tick(fps)
+
   screen.blit(background, (0, 0))
+  screen.blit(floor, (scroll, background.get_height()))
+  scroll -= scroll_speed
+
+  if abs(scroll) > 35:
+    scroll = 0
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
